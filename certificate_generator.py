675f2ce7b908da2940cec2ca695169f04fac51e2
Prabhash <68852615+Prabhash3@users.noreply.github.com>
2021-04-30 21:51:28 +0530

    Update certificate_generator.py
    Covid yodha certificate complete

--- certificate_generator.py
+++ certificate_generator.py
@@ -11,18 +11,19 @@ df = pd.read_csv('certificate.csv') #Read from .csv file
 font = ImageFont.truetype('fonts/MLSJN.ttf',60) #Font style and size
 
 for index,j in df.iterrows():
-    img = Image.open('certificate.jpeg') #Open the .jpeg file
+    img = Image.open('certificate.jpg') #Open the .jpg file
     draw = ImageDraw.Draw(img)
 
-    if(len(j['name'])<=10):
-        draw.text(xy=(520,400),text='{}'.format(j['name']),fill=(0,0,0),font=font) #for name of length <= 10
+    if len(j['name'])<=10:
+        draw.text(xy=(500,400),text='{}'.format(j['name']),fill=(0,0,0),font=font) #for name of length <=10
 
-    elif(len(j['name'])>10 and len(j['name'])<=16):
-        draw.text(xy=(430,400),text='{}'.format(j['name']),fill=(0,0,0),font=font) #for name of length > 10 ad <= 16
+    elif len(j['name'])>10 and len(j['name'])<=13:
+        draw.text(xy=(430,400),text='{}'.format(j['name']),fill=(0,0,0),font=font) #for name of length > 10 and <=13
 
-    elif(len(j['name'])>16):
-        draw.text(xy=(320,400),text='{}'.format(j['name']),fill=(0,0,0),font=font) #for name of length > 16
-        
+    elif len(j['name'])>13 and len(j['name'])<=16:
+        draw.text(xy=(400,400),text='{}'.format(j['name']),fill=(0,0,0),font=font) #for name of length>13 and <=16
 
-    img.save('Generated-Certificates/{}.jpeg'.format(j['name']))
+    elif len(j['name'])>16:
+        draw.text(xy=(320,400),text='{}'.format(j['name']),fill=(0,0,0),font=font) #for name of length>16
 
+    img.save('Generated-Certificates/{}.jpg'.format(j['name']))
